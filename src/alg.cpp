2427aec3cd8aa429f21fc50bce342d4457b6052e
KantinovV <125280143+KantinovV@users.noreply.github.com>
2023-05-14 19:10:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0;
+  int right = size - 1;
+  int amount = 0;
+  int mid = 0;
+  int mid2 = 0;
+  while (right >= left) {
+    mid = (right + left) / 2;
+    if (arr[mid] == value) {
+      amount++;
+      mid2 = mid;
+      while (arr[mid + 1] == value) {
+        amount++;
+        mid++;
+      }
+      while (arr[mid2 - 1] == value) {
+        amount++;
+        mid2--;
+      }
+      return amount;
+    } else if (value < arr[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return amount;
 }
